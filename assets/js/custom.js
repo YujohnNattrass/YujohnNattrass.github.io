437d320f68da05d48f185d5313220f364b2f66f6
Yujohn Nattrass <yujohn.natt@gmail.com>
2022-01-28 17:43:13 -0500

    add umami event listeners

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.img-link').forEach(e => {
    e.addEventListener('click', () => {
+    umami('able picture click');
      window.open('https://www.able-test.com/case', '_blank');
-   })
-  })
-})
+   });
+  });
+
+  document.getElementById('linkedin').addEventListener('click', () => {
+    umami('linkedin button click');
+  });
+
+  document.getElementById('github').addEventListener('click', () => {
+    umami('github button click');
+  });
+
+  document.getElementById('resume').addEventListener('click', () => {
+    umami('resume button click');
+  });
+});
